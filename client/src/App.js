6a33e298d2ec04cfa376a98a0e74a58797bb3cfd
Jaime <jaime.huarsaya.rivera@gmail.com>
2018-07-31 23:09:32 -0500

    Adding Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
+
+/*
+import User from './components/User';
+import Repo from './components/Repo';
+*/
+
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import {ApolloClient} from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
+import {createHttpLink} from 'apollo-link-http';
+
+/*Authentification*/
+const token = 'TOKEN';
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${token}`,
+  },
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <h1>GitHub Users</h1>
     );
   }
 }
