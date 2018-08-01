6e9aa537bca3454608d0b188fcb11182907bc3db
Jaime <jaime.huarsaya.rivera@gmail.com>
2018-08-01 00:26:31 -0500

    Adding Repositories Query to Repo Component

--- client/src/components/Repo.js
+++ client/src/components/Repo.js
@@ -1,4 +1,53 @@
 import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+
+const fragments = gql`
+	fragment repos on Node {
+  ...on User{
+    repositories(first: 20, privacy: PUBLIC, orderBy: {field:NAME,direction :ASC} ){
+      edges{
+        node{
+          ...on Repository{
+            name
+            id
+            description
+            pullRequests(first:1){
+              totalCount
+            }
+          }
+        }
+      }
+    }
+  }
+  ...on Organization{
+    repositories(first: 20, privacy: PUBLIC, orderBy: {field:NAME,direction :ASC} ){
+      edges{
+        node{
+          ...on Repository{
+            name
+            id
+            description
+            pullRequests(first:1){
+              totalCount
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+const getRepoData = gql`
+	query($userid: ID!){
+  		node(id: $userid){
+    		... repos
+  		}
+	}
+	${fragments}
+
+`;
 
 class Repo extends Component {
 
